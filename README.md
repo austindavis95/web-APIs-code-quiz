74740756e6ecd500f3bde4be667ecb13c75c06bf
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-05-30 00:07:44 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # web-APIs-code-quiz
+
+##Name and Contact Information
+
+##Austin Hunter Davis. email - (adavis102595@yahoo.com) 
+github - (https://github.com/austindavis95)
+
+##Project Links:
+
+github-web API quiz:n  https://github.com/austindavis95/web-APIs-code-quiz
+Live URL: https://austindavis95.github.io/web-APIs-code-quiz
+
+
+##web-APIs-code-quiz
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+
+
+##Introduction
+
+In this weeks challengeI applied skills ive leanred with APIs and Java script to build a coding quiz. This application allows you to take a timed based data coding quiz. The timer keeps track of of how long it takes you to completet the quiz. While also penalizing to you for answering a question incorrectly by subtracting 10 seconds off your score time. Answer all questions as fast as you can to get the highest score. quiz will end after all questions have been answered of the timer runs out. After completing the quiz add your name to record your highscore. Then check the leader board to see your rankings!
+
+
+##Acceptance Criteria
+
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,95 +1,111 @@
+// header elements (high score button and time)
+const header = document.querySelector('header');
+const highScoresButton = document.querySelector('#view-high-scores');
+const timerEl = document.querySelector('#timeLeft');
+
+// Initial Quiz Page
+const startQuizSection = document.querySelector('#start-quiz-section');
+const startQuizMessage = document.querySelector('#startQuizMessage');
+const startBtn = document.querySelector('#start-quiz');
+
+// Quiz Questions Section
+const quizSection = document.querySelector('#quiz-section');
 const questionEl = document.querySelector('#question-title');
 const choicesEl = document.querySelector('#choices');
-const timerEl = document.querySelector('#timeLeft');
-const startBtn = document.querySelector('#start-quiz');
-const feedbackEl = document.getElementById('feedback');
-const initialsEl = document.getElementById('enterInitials')
-const submitBtn = document.getElementById('submit');
+const feedbackEl = document.querySelector('#feedback');
 
+// Save Your Score Prompt
+const finalScoreSection = document.querySelector('#final-score-section');
+const finalScoreEl = document.querySelector("#finalScoreDisplay");
+const initialsEl = document.querySelector('#initials')
+const submitBtn = document.querySelector('#submit');
 
-const scoreText = document.querySelector('#score');
+// High Scores Page
+const highScoreSection = document.querySelector('#high-score-section');
+const highScoreList = document.querySelector('#highscores');
+const goBackButton = document.querySelector('#go-back-button');
+const clearHighScoresButton = document.querySelector('#clear-high-scores');
 
-
-
+// Variables
 let acceptingAnswers = true;
-let Score = 0 
-let questionsCounter = 0
-let availableQuestions = []
+let Score = 0;
+let questionsCounter = 0;
+let availableQuestions = [];
+let savedScores = [];
 
 let questions = [
     {
        question:  'Arrays can be used to store the following:',
        choices:   ['Numbers of strings', 'Other arrays', 'Booleans', 'All of the Above'],
        answer:    'All of the Above',
-
     },
     {
         question:  'The condidtion statement if/else is enclosed with the following:',
         choices:   ['Commas','Curly brackets','Quotes','Parentheses'],
         answer:    'Parentheses',
- 
+
      },
      {
         question:  'Commonly used datatypes DO NOT include?:',
         choices:   ['Strings', 'Boolean', 'Alerts', 'Numbers'],
         answer:    'Alerts',
-
      },
      {
         question:  'A very useful tool to debug arrays is:',
         choices:   ['Javascript','Terminal/bash','for loops','Console.log'],
         answer:    'Console.log',
-
     },
     {
         question:  'Strings must be enclosed within ____ when being assigned to variables.',
         choices:   ['Commas','Curly brackets','Quotes','Paranethesese'],
         answer:    'Quotes',
-
     },
+];
 
-]
+var currentQuestionIndex;
+var time;
+var timerId; 
 
-    var currentQuestionIndex = 0;
-    var time = questions.length * 15;
-    var timerId; 
-    
 
-    function startQuiz (){
-        timerId = setInterval(startClock, 1000);
-        timerEl.textContent = time;
-        setQuestion();
-    }
+function startQuiz() {
+    startQuizSection.setAttribute("class", "hide");
+    currentQuestionIndex = 0;
+    time = questions.length * 15;
+    timerId = setInterval(startClock, 1000);
+    timerEl.textContent = time;
+    setQuestion();
+}
 
-    function startClock(){
-        time--;
-        timerEl.textContent = time;
+function startClock() {
+    time--;
+    timerEl.textContent = time;
 
     if (time <= 0){
         quizEnd();
     }
+}
 
-    }
+// Print question to page during quiz
+function setQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    questionEl.textContent = currentQuestion.question;
 
-    function setQuestion (){
-        var currentQuestion = questions[currentQuestionIndex];
-        questionEl.textContent = currentQuestion.question;
+    choicesEl.innerHTML = "";
 
-        choicesEl.innerHTML = "";
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = choice;
+        choiceBtn.onclick = choiceClick;
+        choicesEl.appendChild(choiceBtn);
+    });
+}
 
-        currentQuestion.choices.forEach(function(choice, i) {
-            var choiceBtn = document.createElement("button");
-            choiceBtn.setAttribute("class", "choice");
-            choiceBtn.setAttribute("value", choice);
-            choiceBtn.textContent = i + choice;
-            choiceBtn.onclick = choiceClick;
-            choicesEl.appendChild(choiceBtn);
-        })
-    }
-
-    function choiceClick (){
-        if (this.value !== questions[currentQuestionIndex].answer){
-            time -= 10; 
+// When answer is chosen
+function choiceClick() {
+    if (this.value !== questions[currentQuestionIndex].answer){
+        time -= 10; 
 
         if (time<0){
             time = 0;
@@ -97,33 +113,89 @@ let questions = [
         }
         timerEl.textContent = time;
         feedbackEl.textContent = "wrong";
-        }   
-
-        else{
-            feedbackEl.textContent = "correct";
-        }
-            feedbackEl.setAttribute("class", "feedback");
-        setTimeout(function(){
-            feedbackEl.setAttribute("class", "feedback hide");
-        }, 1000);
-
-        currentQuestionIndex++;
-
-        if (currentQuestionIndex===questions.length){
-            quizEnd();
-        }
-
-        else{
-            setQuestion();
-        }
+    }   
+    else {
+        feedbackEl.textContent = "correct";
     }
+    
+    feedbackEl.setAttribute("class", "feedback");
+    
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
 
-    function quizEnd(){
-        clearInterval(timerId);
+    currentQuestionIndex++;
 
-        var finalScoreEl = document.getElementById("finalScoreDisplay");
-        finalScoreEl.textContent = time;
-
+    if (currentQuestionIndex===questions.length){
+        quizEnd();
     }
+    else{
+        setQuestion();
+    }
+}
 
-    startBtn.onclick = startQuiz;
+//When timer runs out or all questions answered
+function quizEnd() {
+    clearInterval(timerId);
+    finalScoreEl.textContent = "Your final score is: " + time;
+    questionEl.innerHTML = "";
+    choicesEl.innerHTML = "";
+    timerEl.innerHTML = "xxx";
+    displayFinalScoreSection();
+}
+
+// Display final score and prompt to save score
+function displayFinalScoreSection() {
+    header.setAttribute("class", "hide");
+    quizSection.setAttribute("class", "hide");
+    finalScoreSection.setAttribute("class", "");
+}
+
+// Show list of high scores
+function displayHighScores() {
+    header.setAttribute("class", "hide");
+    quizSection.setAttribute("class", "hide");
+    finalScoreSection.setAttribute("class", "hide");
+    highScoreSection.setAttribute("class", "");
+}
+
+// Save input from final score page
+function saveHighScore(score) {
+    savedScores.push([initials.value, time]);
+    initials.value = "";
+    printHighScores();
+    displayHighScores();
+}
+
+// When score saved, order the scores and print
+function printHighScores() {
+    highScoreList.innerHTML = "";
+    sortedScores = savedScores.sort(function(a, b) { return b[1] - a[1]; });
+    savedScores.forEach((score, i) => {
+        highScoreList.innerHTML += "<li>" + (++i) + ". " + score[0] + " - " + score[1] + "</li>";
+    })
+}
+
+// Clear saved scores
+function clearHighScores() {
+    savedScores = [];
+    highscores.innerHTML = "";
+}
+
+//When go back button is pressed from high scores page
+function goBack() {
+    header.setAttribute("class", "");
+    if (timerEl.innerHTML == "xxx") {
+        startQuizSection.setAttribute("class", "");
+    }
+    
+    quizSection.setAttribute("class", "");
+    highScoreSection.setAttribute("class", "hide");
+}
+
+// Button event listeners
+highScoresButton.onclick = displayHighScores;
+submitBtn.onclick = saveHighScore;
+goBackButton.onclick = goBack;
+clearHighScoresButton.onclick = clearHighScores;
+startBtn.onclick = startQuiz;
